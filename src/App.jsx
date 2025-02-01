f33e68d1e5cb3b0fb9129de357394a958f49b083
MarwanabdalR <marwanabdalrady13@gmail.com>
2025-02-01 06:32:17 +0200

    Home
    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+
 import Layout from './components/Layout/Layout.jsx'
 import Home from './pages/Home/Home.jsx'
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,30 @@
-
+import "./Home.css";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <div className="bg-teal-500 py-24">
+        <div className="flex items-center justify-center py-16">
+          <img
+            className="w-60"
+            src="https://routeegy.github.io/startFramework/assets/images/avataaars.svg"
+            alt="avatar"
+          />
+        </div>
+        <h1 className="text-center text-5xl my-5 text-white font-bold">
+          start Framework
+        </h1>
+        <div className="flex items-center justify-center my-2">
+          <div className="line"></div>
+          <div className="mb-2">
+            <i className="fa-solid fa-star fa-xl text-white"></i>{" "}
+          </div>
+          <div className="line"></div>
+        </div>
+        <h1 className="text-center text-xl my-5 text-white font-thin">
+          Graphic Artist - Web Designer - Illustrator
+        </h1>
+      </div>
+    </>
+  );
 }
